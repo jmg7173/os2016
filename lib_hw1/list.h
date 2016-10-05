8a1eda3366991a81f5becf035fa2f6bdbaa5ff46
jmg7173 <jmg7173@sogang.ac.kr>
2016-10-05 17:37:24 +0900

    What is happened? Nothing to change?

--- lib_hw1/list.h
+++ lib_hw1/list.h
@@ -145,7 +145,7 @@ bool list_empty (struct list *);
 
 /* Miscellaneous. */
 void list_reverse (struct list *);
-
+
 /* Compares the value of two list elements A and B, given
    auxiliary data AUX.  Returns true if A is less than B, or
    false if A is greater than or equal to B. */
@@ -165,4 +165,7 @@ void list_unique (struct list *, struct list *duplicates,
 struct list_elem *list_max (struct list *, list_less_func *, void *aux);
 struct list_elem *list_min (struct list *, list_less_func *, void *aux);
 
+/* New function */
+void list_swap (struct list_elem *a, struct list_elem *b);
+void list_shuffle (struct list *list);
 #endif /* lib/kernel/list.h */
