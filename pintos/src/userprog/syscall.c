82533ca3ce0a87fcd4e8827d82e20276b645b83d
jmg7173 <jmg7173@sogang.ac.kr>
2016-10-27 03:59:08 +0900

    Added skeletal function , and so on, for systemcall.

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -1,8 +1,11 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include <console.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/input.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -13,6 +16,7 @@ syscall_init (void)
 }
 
 // XXX : Modify f's eax, esp and so on?? Reference intr_handler
+// XXX : To Pass test, implement exec system call.
 // TODO : implement write, read system call first!
 /* Interrupt handle function */
 static void
@@ -22,18 +26,38 @@ syscall_handler (struct intr_frame *f)
   // XXX : How can I handle system call? make base structure.
   // use switch-case?
   // Interrupt vector no is known as 0x30
+  if(!f->esp || !is_user_vaddr(f->esp))
+    usercall_exit(-1);
+  printf("system calling! syscall num : %d\n",syscallnum);
+  hex_dump(f->esp,f->esp,212,true);
   switch(syscallnum)
     {
       /* implement later */
       /* Project 2 */
     case SYS_HALT:
+      usercall_halt();
+      break;
     case SYS_EXIT:
+      usercall_exit(*(int*)(f->esp+4));
+      break;
     case SYS_EXEC:
+      break;
     case SYS_WAIT:
+      break;
+      /* Not implement */
     case SYS_CREATE:
     case SYS_REMOVE:
     case SYS_OPEN:
     case SYS_FILESIZE:
+      break;
+    case SYS_READ:
+      break;
+    case SYS_WRITE:
+      f->eax = usercall_write(*(int*)(f->esp+4),
+			      *(const void**)(f->esp+8),
+			      *(unsigned*)(f->esp+12));
+      break;
+      /* Not implement */
     case SYS_SEEK:
     case SYS_TELL:
     case SYS_CLOSE:
@@ -49,9 +73,78 @@ syscall_handler (struct intr_frame *f)
     default:
       break;
       /* implement first */
-    case SYS_READ:
-    case SYS_WRITE:
-      break;
+    
+
     }
   thread_exit ();
+}
+
+void
+usercall_halt(void)
+{
+  shutdown_power_off();
+}
+
+void
+usercall_exit(int status UNUSED)
+{
+  printf("exit(%d)\n",status);
+}
+
+pid_t
+usercall_exec(const char *file UNUSED)
+{
+}
+
+int
+usercall_wait(pid_t pid UNUSED)
+{
+}
+
+bool
+usercall_remove(const char *file UNUSED)
+{
+}
+
+int
+usercall_open(const char *file UNUSED)
+{
+}
+
+int
+usercall_filesize(int fd UNUSED)
+{
+}
+
+int
+usercall_read(int fd, void *buffer, unsigned size)
+{
+  if(fd == 0)
+    {
+    }
+}
+
+int
+usercall_write(int fd, const void *buffer, unsigned size)
+{
+  if(fd == 1)
+    {
+      putbuf(buffer, size);
+      return size;
+    }
+}
+
+void
+usercall_seek(int fd UNUSED, unsigned position UNUSED)
+{
+}
+
+unsigned
+usercall_tell(int fd UNUSED)
+{
+}
+
+void
+usercall_close(int fd UNUSED)
+{
 }
--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -1,6 +1,20 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "lib/user/syscall.h"
+
 void syscall_init (void);
 
+void usercall_halt(void);
+void usercall_exit(int status);
+pid_t usercall_exec(const char *file);
+int usercall_wait(pid_t pid);
+bool usercall_remove(const char *file);
+int usercall_open(const char *file);
+int usercall_filesize(int fd);
+int usercall_read(int fd, void *buffer, unsigned size);
+int usercall_write(int fd, const void *buffer, unsigned size);
+void usercall_seek(int fd, unsigned position);
+unsigned usercall_tell(int fd);
+void usercall_close(int fd);
 #endif /* userprog/syscall.h */
