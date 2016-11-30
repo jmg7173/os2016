a8bc6050b04dc0fff2b6a5a913ec2387774386fd
jmg7173 <jmg7173@sogang.ac.kr>
2016-11-30 16:30:29 +0900

    Alarm Clock & timer sleep. sleep busy wait -> thread blocking

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -3,11 +3,13 @@
 #include <inttypes.h>
 #include <round.h>
 #include <stdio.h>
+#include <list.h>
 #include "devices/pit.h"
 #include "threads/interrupt.h"
 #include "threads/synch.h"
 #include "threads/thread.h"
-  
+#include "threads/malloc.h"
+
 /* See [8254] for hardware details of the 8254 timer chip. */
 
 #if TIMER_FREQ < 19
@@ -24,6 +26,9 @@ static int64_t ticks;
    Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
 
+/* Linked list for sleep */
+static struct list sleep_list;
+
 static intr_handler_func timer_interrupt;
 static bool too_many_loops (unsigned loops);
 static void busy_wait (int64_t loops);
@@ -35,6 +40,7 @@ static void real_time_delay (int64_t num, int32_t denom);
 void
 timer_init (void) 
 {
+  list_init(&sleep_list);
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
 }
@@ -90,10 +96,18 @@ void
 timer_sleep (int64_t ticks) 
 {
   int64_t start = timer_ticks ();
-
+  struct sleep_elem *se = malloc(sizeof(struct sleep_elem));
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  
+  enum intr_level old_level = intr_disable();
+
+  se->t = thread_current();
+  se->tick = start + ticks;
+
+  list_push_back(&sleep_list, &se->elem);
+  thread_block();
+
+  intr_set_level(old_level);
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -171,6 +185,17 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
   ticks++;
+  struct list_elem *e;
+  for(e = list_begin(&sleep_list); e != list_end(&sleep_list);
+      e = list_next(e))
+    {
+      struct sleep_elem *se = list_entry(e, struct sleep_elem, elem);
+      if(timer_ticks() >= se->tick)
+	{
+	  list_remove(&se->elem);
+	  thread_unblock(se->t);
+	}
+    }
   thread_tick ();
 }
 
